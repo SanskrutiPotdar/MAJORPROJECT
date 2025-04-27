954903ca4152933121cfd8e2df57afeb6a668bef
Sanskruti Potdar <SanskrutiPotdar>
2025-04-27 21:21:22 +0530

    updated

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,15 @@
-mapboxgl.accessToken = mapToken;
-
+console.log(mapToken);
+    mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        center: [77.2090, 28.6139],// starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9,// starting zoom
+        container: "map", // container ID
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        zoom: 8  // starting zoom
     });
 
-    // const marker = new mapboxgl.Marker()
-    // .setLngLat([]) //Listing.geometry.coordinates
-    // .addTo(map);
-console.log(coordinates);
+    
+const marker = new mapboxgl.Marker({color:'red'})
+.setLngLat(listing.geometry.coordinates)
+.setPopup(new mapboxgl.Popup({offset:25})
+.setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p`))
+.addTo(map);
+  
